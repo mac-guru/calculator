abc6c06212926664730ddc31d03b3774ddef7d34
mac-guru <amritsapkota11@gmail.com>
2016-01-07 17:36:07 +0545

    adding sounds to button

--- Amrit Calculator/ViewController.swift
+++ Amrit Calculator/ViewController.swift
@@ -7,19 +7,36 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var btnSound: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+     
+        let path = NSBundle.mainBundle().pathForResource("btn", ofType: "wav")
+        let soundURL = NSURL(fileURLWithPath: path!)
+        
+        do{
+            try btnSound = AVAudioPlayer(contentsOfURL: soundURL)
+            btnSound.prepareToPlay()
+        }
+        catch let error as NSError{
+            print(error.debugDescription)
+        }
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func btnPresed(btn:UIButton!){
+    
+        btnSound.play()
     }
-
+    
+    
 
 }
 
